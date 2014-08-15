9feb6746bbd622b4397b6795a8052d6bc08aceaa
GorlJimmy <gorljimmy@gmail.com>
2014-08-16 07:58:18 +0800

    Change-Id: I9ea0071d53deaa5fafb6163d4ff0bc2f4279bcb0

--- linux4pkuss/index.php
+++ linux4pkuss/index.php
@@ -1,33 +1,20 @@
 <?php
 /**
- * Example Application
- *
+ * 欢迎页面首页
  * @package Example-application
- 
-
+*/
 require_once './libs/Smarty.class.php';
 
 $smarty = new Smarty;
-
-//$smarty->force_compile = true;
+$smarty->force_compile = true;
 $smarty->debugging = true;
 $smarty->caching = true;
 $smarty->cache_lifetime = 120;
+session_start();
+if ($_SESSION['user']==null){
+	$smarty->display('index.html');
+	
+}else{
+	$smarty->display('home.html');
+}
 
-$smarty->assign("Name", "Fred Irving Johnathan Bradley Peppergill", true);
-$smarty->assign("FirstName", array("John", "Mary", "James", "Henry"));
-$smarty->assign("LastName", array("Doe", "Smith", "Johnson", "Case"));
-$smarty->assign("Class", array(array("A", "B", "C", "D"), array("E", "F", "G", "H"),
-                               array("I", "J", "K", "L"), array("M", "N", "O", "P")));
-
-$smarty->assign("contacts", array(array("phone" => "1", "fax" => "2", "cell" => "3"),
-                                  array("phone" => "555-4444", "fax" => "555-3333", "cell" => "760-1234")));
-
-$smarty->assign("option_values", array("NY", "NE", "KS", "IA", "OK", "TX"));
-$smarty->assign("option_output", array("New York", "Nebraska", "Kansas", "Iowa", "Oklahoma", "Texas"));
-$smarty->assign("option_selected", "NE");
-
-$smarty->display('index.tpl');
-*/
-
-phpinfo();
---
+++ linux4pkuss/templates_c/8e05ed5a561428f52416a331cb513c743d042f2d.file.footer.tpl.cache.php
@@ -0,0 +1,24 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2014-08-16 07:45:30
+         compiled from "./templates/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:15718856853ee9b9accda54-46199541%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '8e05ed5a561428f52416a331cb513c743d042f2d' => 
+    array (
+      0 => './templates/footer.tpl',
+      1 => 1408065362,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '15718856853ee9b9accda54-46199541',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_53ee9b9ad02576_73357606',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_53ee9b9ad02576_73357606')) {function content_53ee9b9ad02576_73357606($_smarty_tpl) {?></BODY>
+</HTML>
+<?php }} ?>
