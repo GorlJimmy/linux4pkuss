8c3d4fae2c6a6b75972b414478a80b7448350047
Jimmy <gorljimmy@pku.edu.cn>
2015-03-09 18:12:43 -0400

    Signed-off-by: Jimmy <gorljimmy@pku.edu.cn>

--- linux4pkuss/service/NoticeService.class.php
+++ linux4pkuss/service/NoticeService.class.php
@@ -3,11 +3,7 @@
 /*
  * author:Gao Xiaoning
  */
-<<<<<<< HEAD
 class NoticeService {
-=======
-class LinkService {
->>>>>>> branch 'master' of https://github.com/GorlJimmy/linux4pkuss
 	private $PATH;
 	function __construct() {
 		$this->PATH = $_SERVER ['DOCUMENT_ROOT'];
@@ -30,12 +26,12 @@ class LinkService {
 		$result = $db->exec_other ( $sql );
 		return $result > 0 ? true : false;
 	}
-	public function queryArticle($num){
+	public function queryArticle($num) {
 		require_once $this->PATH . '/util/DBHelper.class.php';
 		$sql = "select * from article_tb where num=$num order by id asc";
 		$db = new DBHelper ();
 		$result = $db->exec_query ( $sql );
-		return $result[0];
+		return $result [0];
 	}
 	// 文章列表中显示经过审核的文章
 	public function article_queryAll() {
