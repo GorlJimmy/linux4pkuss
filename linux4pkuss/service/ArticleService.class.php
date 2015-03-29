034a396c91c871c2e8e9ed6227c8c01f1290b4e5
Jimmy <gorljimmy@pku.edu.cn>
2015-03-29 08:03:03 -0400

    add update article function

--- linux4pkuss/service/ArticleService.class.php
+++ linux4pkuss/service/ArticleService.class.php
@@ -54,25 +54,6 @@ class ArticleService {
 		$result = $db->exec_query ( $sql );
 		return $result [0];
 	}
-	// 文章列表中显示经过审核的文章
-	public function article_queryAll() {
-		require_once $this->PATH . '/util/DBHelper.class.php';
-		$sql = "select * from article_tb where isshare=1 order by id asc";
-		$db = new DBHelper ();
-		$result = $db->exec_query ( $sql );
-		return $result;
-	}
-	
-	// 通过id定位到某一文章，用于显示文章内容
-	public function article_queryById($id) {
-		require_once $this->PATH . '/util/DBHelper.class.php';
-		$sql = "select * from article_tb where isshare=1 and id=$id";
-		$db = new DBHelper ();
-		$result = $db->exec_query ( $sql );
-		return $result;
-	}
-	
-	// 点击首页上的一级标题：相关文章(id为4)，显示其下的二级标题
 	public function theme_query() {
 		require_once $this->PATH . '/util/DBHelper.class.php';
 		$sql = "select * from theme_tb where parent_id=4 order by createdate desc";
@@ -89,20 +70,16 @@ class ArticleService {
 		$result = $db->exec_query ( $sql );
 		return $result;
 	}
-	public function teacher_query() {
-		require_once $this->PATH . '/util/DBHelper.class.php';
-		$sql = "select * from theme_tb where id=7";
-		$db = new DBHelper ();
-		$result = $db->exec_query ( $sql );
-		return $result;
-	}
 	// 修改更新文章
-	public function article_upt($id, $title, $createdate, $content, $theme_id) {
+	public function updateArticle($article,$user) {
 		require_once $this->PATH . '/util/DBHelper.class.php';
-		$sql = "update article_tb set title=" . "'" . $title . "'" . ",createdate=" . "'" . $createdate . "'" . ",content=" . "'" . $content . "'" . ",theme_id=" . $theme_id . " where id=" . $id;
+		$num = time () . rand ( 1, 10000 );
+		$now = date ( 'Y-m-d h:m:s', time () );
+		$user_id = intval ( $user ['id'] );
+		$sql = "update article_tb set title=" . "'" . $article[title] . "'" . ",createdate=" . "'" . $now . "'" . ",content=" . "'" . $article[content] . "'" . ",user_id=" . $user_id . " where num=" . $article[num];
 		$db = new DBHelper ();
 		$result = $db->exec_other ( $sql );
-		return $result;
+		return $result > 0 ? true : false;
 	}
 	// 删除文章
 	public function deleteArticle($num) {
@@ -110,7 +87,6 @@ class ArticleService {
 		$sql = "delete from article_tb where num in" . "(" . $num . ")";
 		$db = new DBHelper ();
 		$result = $db->exec_other ( $sql );
-		
 		return $result > 0 ? true : false;
 	}
 }
